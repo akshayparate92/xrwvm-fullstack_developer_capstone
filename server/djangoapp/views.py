9ca292d19e7d4cf81d056db7b10bdedcc8b36793
akshayparate92 <akshayparate92@gmai.com>
2026-08-14 23:46:15 -0400

    view and urls files linting code done

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,6 @@
 import json
 import logging
 
-from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import JsonResponse
